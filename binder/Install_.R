954c7165fabaf226d10b21946e49da672f9b1706
JuKo007 <JuKo007@users.noreply.github.com>
2018-07-19 14:56:53 +0200

    Delete Install_.R

--- binder/Install_.R
+++
@@ -1,3 +0,0 @@
-install.packages("randomNames", repos='http://cran.us.r-project.org')
-install.packages("igraph", repos='http://cran.us.r-project.org')
-install.packages("RColorBrewer", repos='http://cran.us.r-project.org')
